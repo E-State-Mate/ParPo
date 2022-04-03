3382c2583ded2d7cf41fb3bec498aaa1c7e62ee8
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2022-04-03 19:32:55 -0400

    Implemented holdingContext

---
+++ parpo-profile/src/Lib/HoldingContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState, useContext } from "react";
+import axios from "axios";
+
+export const HoldingContext = createContext({});
+
+const HoldingProvider = ({ children }) => {
+  const [Holdings, setHoldings] = useState([]);
+  const [Holding, setHolding] = useState({});
+
+  // GET/holdings
+  const getAllHoldings = async () => {
+    const response = await axios.get("http://localhost:5000/auth-development-92670/us-central1/getHoldings");
+    setHoldings(response.data);
+
+    return response;
+  };
+
+  return <HoldingContext.Provider value={{ getAllHoldings }}>{children}</HoldingContext.Provider>;
+};
--- parpo-profile/src/Pages/HoldingList.jsx
+++ parpo-profile/src/Pages/HoldingList.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import { Card, CardHeader, Grid } from '@mui/material'
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import ButtonBase from '@mui/material/ButtonBase';
-import PropertyType from '../Components/HoldingList/PropertyType';
-import HoldingCard from '../Components/HoldingList/HoldingCard.js'
-import holdingData from '../Lib/holdingData.json'
-import {propertyTypes} from '../Lib/propertyTypeData'
+import React, { useEffect, useContext } from "react";
+import { Card, CardHeader, Grid } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import ButtonBase from "@mui/material/ButtonBase";
+import PropertyType from "../Components/HoldingList/PropertyType";
+import HoldingCard from "../Components/HoldingList/HoldingCard.js";
+import holdingData from "../Lib/holdingData.json";
+import { propertyTypes } from "../Lib/propertyTypeData";
+import { HoldingContext } from "../Lib/HoldingContext";
 
 // When you make components (Filter Type card, Holding card, etc), put them in Components/HoldingList to help keep things organized
 
-
 const HoldingList = () => {
+  const { Holdings, getAllHoldings } = useContext(HoldingContext);
+
+  useEffect(() => {
+    getAllHoldings();
+  });
 
   return (
-    <div id='holding-list-container'>
-      <Grid container width='90%' style={{margin: 'auto'}}>
+    <div id="holding-list-container">
+      <Grid container width="90%" style={{ margin: "auto" }}>
         {/* Property Filter Boxes */}
         <Grid item lg />
         <Grid item md={4} lg={2}>
-          <p style={{margin: '1rem'}}>Property Type</p>
-          {propertyTypes.map(property => ( <PropertyType property={property}/> ))}
+          <p style={{ margin: "1rem" }}>Property Type</p>
+          {propertyTypes.map((property) => (
+            <PropertyType property={property} />
+          ))}
         </Grid>
         <Grid item md={8} lg={5}>
-          <p style={{margin: '1rem'}}>{holdingData.length} Properties</p>
-          {holdingData.map(holding => ( <HoldingCard holding={holding} /> ))}
+          <p style={{ margin: "1rem" }}>{holdingData.length} Properties</p>
+          {holdingData.map((holding) => (
+            <HoldingCard holding={holding} />
+          ))}
         </Grid>
         <Grid item lg />
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default HoldingList
+export default HoldingList;
