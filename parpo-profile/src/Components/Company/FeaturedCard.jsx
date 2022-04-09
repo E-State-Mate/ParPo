fa20e9dc38a68ae00a3debb96d5c05eb2e5c358a
Zach <zachary.tippit@gmail.com>
2022-04-09 12:00:31 -0600

    Refactored to consider backend schema changes

--- parpo-profile/src/Components/Company/FeaturedCard.jsx
+++ parpo-profile/src/Components/Company/FeaturedCard.jsx
@@ -5,17 +5,17 @@ const FeaturedCard = ({data}) => {
   return (
     <Card className='featured-card' style={{backgroundImage: `url(https://via.placeholder.com/3000/0000FF/808080?Text=Digital.com)`}}> 
         <div className='feat-card-top'>
-            <p>{data.holdingPropertyType} Building</p>
-            <p>{data.holdingSqft} Sq Ft.</p>
-            <p>{data.CreditRating}</p>
+            <p>{data.propertyType} Building</p>
+            <p>{data.sqft} Sq Ft.</p>
+            <p>{data.creditRating}</p>
         </div>
         <div className='feat-card-banner'>
             <div>
-                <p>{data.holdingStreet}</p>
-                <p>Managed by: {data.holdingManager[0]}</p>
+                <p>{data.street}</p>
+                <p>Managed by: {data.manager[0]}</p>
             </div>
             <>
-                <p>{data.holdingEstimatedValue}</p>
+                <p>{data.estimatedValue}</p>
             </>
         </div>
     </Card>
