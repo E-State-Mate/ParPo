a94f34fb4a4deba66fdf082986f8c058da32606f
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2022-04-25 20:16:32 -0400

    HoldingListView > HoldingDetailView connection

--- parpo-profile/src/Components/HoldingList/HoldingCard.js
+++ parpo-profile/src/Components/HoldingList/HoldingCard.js
@@ -5,6 +5,7 @@ import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import React from 'react'
 import { makeStyles } from '@material-ui/core'
+import { Link } from "react-router-dom"
 
 const useStyles = makeStyles({
   image: {
@@ -18,56 +19,56 @@ const HoldingCard = ({holding}) => {
   const classes = useStyles()
   // console.log(holding)
   return (
-    <Card className='prop-list-card'>
-      <Grid container align='left' style={{height: '100%'}}>
-        <Grid item xs={12} sm={4}>
-          <CardMedia
-            className= {classes.image}
-            display= "flex !important"
-            component="img"
-            sx={{ width: '100%' }}
-            image={holding.image}
-            alt="Live from space album cover"
-          />
+    <Link to={`/property/${holding._id}`}>
+      <Card className='prop-list-card'>
+        <Grid container align='left' style={{height: '100%'}}>
+          <Grid item xs={12} sm={4}>
+            <CardMedia
+              className= {classes.image}
+              display= "flex !important"
+              component="img"
+              sx={{ width: '100%' }}
+              image={holding.image}
+              alt="Live from space album cover"
+            />
+          </Grid>
+          {/* Center column -- includes building name, type, address and price */}
+          <Grid item xs={12} sm={4} mt={2}>
+            <div className='prop-list-card-title'>
+              <Typography variant='h6'>{holding.buildingName}</Typography>
+              <Typography variant='body1'>{holding.propertyType.toUpperCase()}</Typography>
+            </div>
+            <div className='prop-list-card-address'>
+              <Typography variant='body2'>{holding.address}</Typography>
+              <Typography variant='body2'>{holding.city}, {holding.state} {holding.zipCode}</Typography>
+            </div>
+            <div className='prop-list-card-list'>
+              <Typography variant='body2'>{holding.price}</Typography>
+            </div>
+          </Grid>
+          {/* Right column -- features specific building data */}
+          <Grid item xs={12} sm={4} className='prop-list-card-right' mt={2}>
+            <div className='prop-list-card-right-info'>
+              <PeopleIcon sx={{marginRight: '1rem'}}/>
+              <Typography variant='body2'>{holding.occupancy}% occupancy</Typography>
+            </div>
+            <div className='prop-list-card-right-info'>
+              <LayersIcon sx={{marginRight: '1rem'}}/>
+              <Typography variant='body2'>{holding.sqft} Sq Ft</Typography>
+            </div>
+            <div className='prop-list-card-right-info'>
+              <AccountBalanceIcon sx={{marginRight: '1rem'}}/>
+              <Typography variant='body2'>{holding.revenue} in 2021 Revenue</Typography>
+            </div>
+            <div className='prop-list-card-right-info'>
+              <StarOutlineIcon sx={{marginRight: '1rem'}}/>
+              <Typography variant='body2'>{holding.rating} Rating</Typography>
+            </div>
+          </Grid>
         </Grid>
-
-        {/* Center column -- includes building name, type, address and price */}
-        <Grid item xs={12} sm={4} mt={2}>
-          <div className='prop-list-card-title'>
-            <Typography variant='h6'>{holding.buildingName}</Typography>
-            <Typography variant='body1'>{holding.propertyType.toUpperCase()}</Typography>
-          </div>
-          <div className='prop-list-card-address'>
-            <Typography variant='body2'>{holding.address}</Typography>
-            <Typography variant='body2'>{holding.city}, {holding.state} {holding.zipCode}</Typography>
-          </div>
-          <div className='prop-list-card-list'>
-            <Typography variant='body2'>{holding.price}</Typography>
-          </div>
-        </Grid>
-
-        {/* Right column -- features specific building data */}
-        <Grid item xs={12} sm={4} className='prop-list-card-right' mt={2}>
-          <div className='prop-list-card-right-info'>
-            <PeopleIcon sx={{marginRight: '1rem'}}/>
-            <Typography variant='body2'>{holding.occupancy}% occupancy</Typography>
-          </div>
-          <div className='prop-list-card-right-info'>
-            <LayersIcon sx={{marginRight: '1rem'}}/>
-            <Typography variant='body2'>{holding.sqft} Sq Ft</Typography>
-          </div>
-          <div className='prop-list-card-right-info'>
-            <AccountBalanceIcon sx={{marginRight: '1rem'}}/>
-            <Typography variant='body2'>{holding.revenue} in 2021 Revenue</Typography>
-          </div>
-          <div className='prop-list-card-right-info'>
-            <StarOutlineIcon sx={{marginRight: '1rem'}}/>
-            <Typography variant='body2'>{holding.rating} Rating</Typography>
-          </div>
-        </Grid>
-      </Grid>
-      <Typography align='center' m={1} sx={{fontSize: '0.625rem'}}>View More</Typography>
-    </Card>
+        <Typography align='center' m={1} sx={{fontSize: '0.625rem'}}>View More</Typography>
+      </Card>
+    </Link>
   )
 }
 
